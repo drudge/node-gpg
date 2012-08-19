1c4430c5a78e801944ae63771167fd108fd34aa8
freewil <sean@eternalrise.com>
2012-08-19 17:33:00 -0400

    make decrypt() consistent with encrypt()
    Also, use gpg exit code to determine if an error has occurred
    since gpg will sometimes use stderr for warning/informational
    messages.

--- test/index.js
+++ test/index.js
@@ -1,7 +1,9 @@
 var assert = require('assert')
-  , gpg = require('../');
+  , gpg = require('../')
+  , encryptedString = "-----BEGIN PGP MESSAGE-----\nVersion: GnuPG v1.4.11 (GNU/Linux)\n\nhIwDx4bi8LXf2bsBBACHzYpE/z18gwHTEQ0NZ9UjVozvslrQnyeNZYTBMZt3uxeQ\nqe2UX6k8M3wiODEuy+sOsOPnhKwdOkPSA2MwdJEcJTRLKsHvex/aysYYZPp7AW6Y\nBs4U7A4K+Jq706C/KJa9A1vRWrmrW92DL5huDppV0vdiqsSACmvnMXJzCJbqP9JH\nAcwEgyhAZdywmXKddMc5slU4knIs3Z+IHpbmkk6SChWlCCAiNXWNHKOf587VLU2G\nAntQfK6CB/3t8U5cNZ0uRwhrs+kwEkI=\n=82nc\n-----END PGP MESSAGE-----"
+  ;
   
-describe('gpg', function(){ 
+describe('gpg', function(){
   
   describe('encrypt', function(){
     it('should encrypt data', function(done){
@@ -16,6 +18,37 @@ describe('gpg', function(){
       gpg.encrypt(mysecret, args, function(err, encrypted){
         assert.ifError(err);
         assert.ok(encrypted.length);
+        done();
+      });
+    });
+  });
+  
+  describe('decrypt', function(){
+    it('should decrypt strings', function(done){
+      var args = [
+        '--trust-model', 'always', // disable trust checking for purposes of running tests
+        '--no-use-agent'
+      , '--batch'
+      ];
+      gpg.decrypt(encryptedString, args, function(err, decrypted){
+        assert.ifError(err);
+        assert.ok(decrypted.length);
+        assert.equal(decrypted.toString('utf8'), 'Hello World\n');
+        done();
+      });
+    });
+    
+    it('should decrypt Buffers', function(done){
+      var args = [
+        '--trust-model', 'always', // disable trust checking for purposes of running tests
+        '--no-use-agent'
+      , '--batch'
+      ];
+      var encryptedBuffer = new Buffer(encryptedString);
+      gpg.decrypt(encryptedBuffer, args, function(err, decrypted){
+        assert.ifError(err);
+        assert.ok(decrypted.length);
+        assert.equal(decrypted.toString('utf8'), 'Hello World\n');
         done();
       });
     });
